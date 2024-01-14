b5b0254fcb2b6df20ed679637a205452eb57fbbf
Tuong <tuong.wall123@gmail.com>
2024-01-14 16:00:38 +0700

    design layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="trello-master">
+      <nav className='navbar app'>App bar</nav>
+      <nav className='navbar board'>Board bar</nav>
+      <div className='board-columns'>
+
+        <div className='column'>
+          <header>BrainStorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design and Research
+            </li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+
+
+            <footer>Add another card</footer>
+          </ul>
+        </div>
+
+        <div className='column'>
+          <header>Two</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design and Research
+            </li>
+            <li>Hello</li>
+            <footer>Add another card</footer>
+          </ul>
+        </div>
+
+        <div className='column'>
+          <header>Three</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design and Research
+            </li>
+            <li>Hello</li>
+            <footer>Add another card</footer>
+          </ul>
+        </div>
+
+      </div>
     </div>
   );
 }
